aa2ddf9dbe8ca4102d9dc6fdd2729942c745f1e8
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-03-04 17:59:06 -0300

    :sparkles: Commit inicial

---
+++ src/main/java/com/projeto/board_tarefa/BoardTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.board_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/board_tarefa/BoardTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.board_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
